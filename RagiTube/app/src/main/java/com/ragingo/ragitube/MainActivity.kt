0941250b25b5bb617a37cf11603ee8dea82a53db
ragingo <save.the.world.crush@gmail.com>
2020-05-12 23:34:50 +0900

    動画ID固定で再生

--- RagiTube/app/src/main/java/com/ragingo/ragitube/MainActivity.kt
+++ RagiTube/app/src/main/java/com/ragingo/ragitube/MainActivity.kt
@@ -2,10 +2,16 @@ package com.ragingo.ragitube
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.youtube.player.YouTubeStandalonePlayer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val apiKey = getString(R.string.google_api_key)
+        val videoId = "mw5VIEIvuMI"
+        val intent = YouTubeStandalonePlayer.createVideoIntent(this, apiKey, videoId);
+        startActivity(intent);
     }
 }
