d8496b220c69ad01a2705f5ce525083c53b2d1fc
ragingo <save.the.world.crush@gmail.com>
2020-05-30 04:46:37 +0900

    viewmodel 追加

---
+++ RagiTube/app/src/main/java/com/ragingo/ragitube/viewModels/SettingsViewModel.kt
@@ -0,0 +1,6 @@
+package com.ragingo.ragitube.viewModels
+
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel : ViewModel() {
+}
--- RagiTube/app/src/main/java/com/ragingo/ragitube/views/ui/SettingsFragment.kt
+++ RagiTube/app/src/main/java/com/ragingo/ragitube/views/ui/SettingsFragment.kt
@@ -3,17 +3,22 @@ package com.ragingo.ragitube.views.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.ragingo.ragitube.R
 import com.ragingo.ragitube.databinding.FragmentSettingsBinding
+import com.ragingo.ragitube.viewModels.SettingsViewModel
 import kotlinx.android.synthetic.main.fragment_settings.*
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
+    private val vm: SettingsViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val binding = FragmentSettingsBinding.bind(view)
         binding.lifecycleOwner = this
+        binding.vm = vm
 
         button_license.setOnClickListener {
             findNavController().navigate(R.id.action_nav_settings_to_nav_license)
